e8674b2049f3915e87e4246cb4069d937219c3ff
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-06-21 18:28:07 +0530

    Clean up deleted files from project

--- Design Patterns/CREATIONAL-DESIGN-PATTERNS/Factory/src/com/coffeepoweredcrew/factorymethod/conccretenotification/EmailNotification.java
+++
@@ -1,9 +0,0 @@
-package com.coffeepoweredcrew.factorymethod.conccretenotification;
-
-public class EmailNotification implements NotificationProduct {
-	
-	@Override
-	public void notifyUser() {
-		System.out.println("Sendinng an Email Notification");
-	}
-}
