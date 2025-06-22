c1ea30c8c481ed2d29c3f3c1e70dc041e7614cff
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2025-06-22 15:21:39 +0530

    Added Prototype design pattern

--- Design_Patterns/src/main/java/creational/design/pattern/prototype/Client.java
+++ Design_Patterns/src/main/java/creational/design/pattern/prototype/Client.java
@@ -1,4 +1,19 @@
 package creational.design.pattern.prototype;
 
 public class Client {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        Book book = new Book("Design Patterns" , "GoF");
+
+        // Clone the object
+        Book copyBook = null;
+        try {
+            copyBook = book.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new CloneNotSupportedException("Clone Not Supported ");
+        }
+        System.out.println("Original Book: " + book);
+        System.out.println("Copy Book: " + copyBook);
+    }
 }
--- Design_Patterns/src/main/java/creational/design/pattern/prototype/Prototype.java
+++ Design_Patterns/src/main/java/creational/design/pattern/prototype/Prototype.java
@@ -1,4 +1,6 @@
 package creational.design.pattern.prototype;
 
-public interface Prototype {
+public interface Prototype extends Cloneable{
+
+    Prototype clone() throws CloneNotSupportedException;
 }
