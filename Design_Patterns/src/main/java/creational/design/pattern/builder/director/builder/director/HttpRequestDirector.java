e088f2ec1f0fa58057dff2fcdad2f09be172d3b6
saddam <saddam8101.sk@gmail.com>
2026-02-01 23:41:26 +0530

    added Http request director Builder Pattern

---
+++ Design_Patterns/src/main/java/creational/design/pattern/builder/director/builder/director/HttpRequestDirector.java
@@ -0,0 +1,24 @@
+package creational.design.pattern.builder.director.builder.director;
+
+import creational.design.pattern.builder.director.builder.builder.HttpRequest;
+
+public class HttpRequestDirector {
+
+    public static HttpRequest createGetRequest(String url){
+
+        return new HttpRequest.HttpRequestBuilder()
+                .withUrl(url)
+                .withMethod("GET")
+                .build();
+    }
+    public static HttpRequest createJsonPostRequest(String url, String jsonBody){
+
+        return new HttpRequest.HttpRequestBuilder()
+                .withUrl(url)
+                .withMethod("POST")
+                .withHeaders("Content-Type: application/json")
+                .withHeaders("Accept: application/json")
+                .withBody(jsonBody)
+                .build();
+    }
+}
